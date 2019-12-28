f0b47745ef492f8a11ca97cc727620c479551dbe
Ihor Samilo <Ihor33804@gmail.com>
2019-12-28 13:34:25 +0200

    first commit

---
+++ precache-manifest.07f8f3d55029380c7fecbc32d9238ea9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "27b19a43a4d825b5cd72830f3b55e4c3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d7c23aa0e9fea496388f",
+    "url": "/static/css/main.e787b0b4.chunk.css"
+  },
+  {
+    "revision": "5b4f407525df3bc98b14",
+    "url": "/static/js/2.9b986f6a.chunk.js"
+  },
+  {
+    "revision": "9a36add8c2bc93b3c60869d6ef279dc0",
+    "url": "/static/js/2.9b986f6a.chunk.js.LICENSE"
+  },
+  {
+    "revision": "d7c23aa0e9fea496388f",
+    "url": "/static/js/main.5a7efd10.chunk.js"
+  },
+  {
+    "revision": "f6171dc0af36091425ac",
+    "url": "/static/js/runtime-main.46830b67.js"
+  }
+]);
